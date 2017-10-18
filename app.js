d75f25dd83ebd30b20adfc8288106531d3c0dc38
drumicube <drumicube@gmail.com>
2017-10-18 20:43:22 +0200

    Add a Basic Command Handler

--- app.js
+++ app.js
@@ -1,21 +1,15 @@
-// Load up the discord.js library
-const Discord = require("discord.js");
 
-// http://countdownjs.org
-const Countdown = require("countdown");
+const Discord     = require("discord.js");        // Load up the discord.js library
+const fs          = require("fs");
 
-// https://momentjs.com/
-const Moment = require("moment");
 
 // This is your client. Some people call it `bot`, some people call it `self`,
-// some might call it `cootchie`. Either way, when you see `client.something`, or `bot.something`,
-// this is what we're refering to. Your client.
 const client = new Discord.Client();
 
 // Here we load the config.json file that contains our token and our prefix values.
 const config = require("./config.json");
-// config.token contains the bot's token
-// config.prefix contains the message prefix.
+
+// -------------------- event ready ----------------------
 
 client.on("ready", () => {
   // This event will run if the bot starts, and logs in, successfully.
@@ -28,11 +22,15 @@ client.on("ready", () => {
     client.user.setGame(`on ${client.guilds.size} server`);
 });
 
+// -------------------- event guildCreate ----------------------
+
 client.on("guildCreate", guild => {
   // This event triggers when the bot joins a guild.
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
   client.user.setGame(`on ${client.guilds.size} servers`);
 });
+
+// -------------------- event guildDelete ----------------------
 
 client.on("guildDelete", guild => {
   // this event triggers when the bot is removed from a guild.
@@ -40,47 +38,24 @@ client.on("guildDelete", guild => {
   client.user.setGame(`on ${client.guilds.size} servers`);
 });
 
+// -------------------- event message ----------------------
+
 client.on("message", message => {
   // This event will run on every single message received, from any channel or DM.
 
-  // It's good practice to ignore other bots. This also makes your bot ignore itself
-  // and not get into a spam loop (we call that "botception").
   if(message.author.bot) return;
-
-  // Also good practice to ignore any message that does not start with our prefix,
-  // which is set in the configuration file.
   if(message.content.indexOf(config.prefix) !== 0) return;
 
   // Here we separate our "command" name, and our "arguments" for the command.
-  // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
-  // command = say
-  // args = ["Is", "this", "the", "real", "life?"]
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 
-  // Let's go with a few common example commands! Feel free to delete or change those.
-
-  if(command === "ping") {
-    // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
-    // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
-    const m = message.channel.send("Ping?");
-    message.channel.send(`Pong! API Latency is ${Math.round(client.ping)}ms`);
-  }
-
-  if(command === "say") {
-    // makes the bot say something and delete the message. As an example, it's open to anyone to use.
-    // To get the "message" itself we join the `args` back into a string with spaces:
-    const sayMessage = args.join(" ");
-    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
-    message.delete().catch(O_o=>{});
-    // And we get the bot to say the thing:
-    message.channel.send(sayMessage);
-  }
-  
-  if(command === "countdown") {
-    var nextDailyReset = Moment.utc().hour(7).minute(0).second(0);
-    if(Moment.utc().isAfter(nextDailyReset)) { nextDailyReset.add(1, 'day'); }
-    message.channel.send(`Daily reset is in ` + Countdown(nextDailyReset).toString());
+  // Basic Command Handler
+  try {
+    let commandFile = require(`./commands/${command}.js`);
+    commandFile.run(client, message, args);
+  } catch (err) {
+    console.error(err);
   }
 
 });
