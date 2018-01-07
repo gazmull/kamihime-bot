1e97196c4dd5b21782e94fb84d0d267b808bcc39
drumicube <drumicube@gmail.com>
2018-01-07 23:47:09 +0100

    add winston logger and daily rotate files

--- events/presenceUpdate.js
+++ events/presenceUpdate.js
@@ -3,13 +3,15 @@
 const   config      = require("../config.json");
 const   db          = require("../dbconfig.js").pool;
 const   moment      = require("moment-timezone");
+const   logger      = require("../logger.js").logger;
 
 exports.run = (client, oldMember, newMember) => {
 
   if(oldMember.presence.status !== newMember.presence.status) {
 
     if ( (newMember.presence.status == "online") || (newMember.presence.status == "offline") ) {
-      //console.log(`${newMember.user.username} is now ${newMember.presence.status}`);
+
+      logger.info(`${newMember.user.username} is now ${newMember.presence.status}`);
       const dateUpdated = moment().format("YYYY-MM-DD HH:mm:ss");
       db.execute('UPDATE `users` SET `user_last_online`=? WHERE `user_discord_id`=?', [dateUpdated, newMember.user.id],
         function(err, results, fields) {
